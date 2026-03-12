15f966ab7e5b1f28dd62b017cf6f606410b75342
jalaxy33 <jalaxy1996@outlook.com>
2026-03-12 17:40:48 +0800

    [fix] enable `init_logger` run in loop

--- src/logging.rs
+++ src/logging.rs
@@ -1,8 +1,14 @@
+use std::sync::Once;
+
+static LOGGER_INIT: Once = Once::new();
+
 pub fn init_logger() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .with_target(false)
-        .with_file(true)
-        .with_line_number(true)
-        .init();
+    LOGGER_INIT.call_once(|| {
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .with_target(false)
+            .with_file(true)
+            .with_line_number(true)
+            .init();
+    });
 }
